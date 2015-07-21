25afea3a0b29d2d40c491cd3b27165b62a6c32d8
Spencer Dailey <sdailey.479@gmail.com>
2015-07-20 23:41:31 -0500

    about to start debugging port

---
+++ package/data/testCommon.js
@@ -0,0 +1,8 @@
+// window.addEventListener('click', function(event) {
+//     var t = event.target;
+//     if (t.nodeName == 'A')
+//       self.port.emit('click-link', t.toString());
+//    }, false);
+window.onload = function(){
+  
+}
